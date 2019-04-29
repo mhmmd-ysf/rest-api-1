468c44c54d4040e3edfbc3a60c51c46b0875556f
mhmmd-ysf <muhammad_yusuf22@outlook.co.id>
2019-04-29 19:55:26 +0700

    Update

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const fs = require('fs')
 const port = process.env.PORT || 3000
 
 const config = {
-  "development": {
+  "production": {
     "username": process.env.USERNAME2,
     "password": process.env.PASSWORD,
-    "database": "RESTAPI",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "database": "d12v6pg0q2tgn8",
+    "host": "ec2-50-19-127-115.compute-1.amazonaws.com",
+    "dialect": "postgres",
+    "port": 5432,
+    "uri": "postgres://fuhbnvczqhenls:ff8714b5e983344917ce4ab05b8a4b9efa546783738cdc1057aacc597e99ee40@ec2-50-19-127-115.compute-1.amazonaws.com:5432/d12v6pg0q2tgn8"
   }
 }
 // fs.writeFileSync( './config/config.json',JSON.stringify(config, null, 2), 'utf8')
